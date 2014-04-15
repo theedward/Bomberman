99fcc175d135eb2b091c25fe3b02b5c4d299813d
André Rodrigues <fury252@gmail.com>
2014-04-15 17:29:20 +0100

    Implemented the Position class;

---
+++ Bomberman/src/com/cmov/bomberman/model/Position.java
@@ -0,0 +1,19 @@
+package com.cmov.bomberman.model;
+
+public class Position {
+	final int x;
+	final int y;
+
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}

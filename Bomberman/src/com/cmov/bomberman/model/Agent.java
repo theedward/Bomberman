dc7d780c34aed44cd860b8e944ae3d1d60c67745
André Rodrigues <fury252@gmail.com>
2014-04-16 12:31:29 +0100

    Implemented the Agent abstract class;

---
+++ Bomberman/src/com/cmov/bomberman/model/Agent.java
@@ -0,0 +1,23 @@
+package com.cmov.bomberman.model;
+
+public abstract class Agent {
+	private Position currentPos;
+	private final Algorithm ai;
+
+	public Agent(Position startingPos, Algorithm ai) {
+		currentPos = startingPos;
+		this.ai = ai;
+	}
+
+	protected Position getCurrentPos() {
+		return currentPos;
+	}
+
+	protected void setCurrentPos(Position p) {
+		this.currentPos = p;
+	}
+
+	protected Algorithm getAlgorithm() {
+		return ai;
+	}
+}

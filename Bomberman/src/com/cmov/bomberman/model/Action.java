35293509b3952902675a0fbd4afa06ce94d06857
André Rodrigues <fury252@gmail.com>
2014-04-16 11:42:22 +0100

    Support for Actions are not really needed.

--- Bomberman/src/com/cmov/bomberman/model/Action.java
+++
@@ -1,7 +0,0 @@
-package com.cmov.bomberman.model;
-
-/**
- * Created by JoãoEduardo on 15-04-2014.
- */
-public class Action {
-}
--- Bomberman/src/com/cmov/bomberman/model/Playable.java
+++ Bomberman/src/com/cmov/bomberman/model/Playable.java
@@ -1,11 +1,8 @@
 package com.cmov.bomberman.model;
 
-import java.util.Map;
-
 /**
  * Created by JoãoEduardo on 15-04-2014.
  */
 public interface Playable {
-        Map<String, Action> possibleActions();
-        void play(State state);
+	void play(State state);
 }

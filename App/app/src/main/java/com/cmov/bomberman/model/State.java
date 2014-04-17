53213d0a458cdabf05f9a59eb35802a5a7ae1c94
Margarida <anamfs91@hotmail.com>
2014-04-17 22:21:50 +0100

    Draw method implemented playAll method implemented but probably not complete

--- App/app/src/main/java/com/cmov/bomberman/model/State.java
+++ App/app/src/main/java/com/cmov/bomberman/model/State.java
@@ -12,8 +12,17 @@ public class State {
 
     }
     // This method will call the method play of each player.
-    // in the end after all plays must check for possible deaths
-    public void playAll() {}
+    // in the end after all plays must check for possible deaths or
+    // objects to be destroyed
+    public void playAll() {
+
+        for(Agent character : objects) {
+            character.play(this);
+            if (character.isDestroyed()) {
+                destroyCharacter(character);
+            }
+        }
+    }
 
     // This method will remove all the given objects from the state.
     public void removeAll(List<Agent> objects) {}

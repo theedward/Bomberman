8ea81aac150c72c1d9cd0e8f4814d043fc13c011
theedward <ed-01@hotmail.com>
2014-04-17 14:36:39 +0100

    Wall class added

---
+++ App/app/src/main/java/com/cmov/bomberman/model/Wall.java
@@ -0,0 +1,30 @@
+package com.cmov.bomberman.model;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by JoãoEduardo on 17/04/2014.
+ */
+public class Wall implements Drawable{
+
+    public static Bitmap[] sprite;
+    private static final int SPRITE_LINE = 13;
+    private static final int SPRITE_COLUMN = 0;
+    private static final int NUM_IMAGES = 1;
+
+    private Position position;
+
+    public Wall(Position position){
+
+        this.position = position;
+        sprite = GameUtils.readCharacterSprite(SPRITE_LINE, SPRITE_COLUMN, NUM_IMAGES);
+    }
+
+    public Position getPosition() { return position; }
+
+    @Override
+    public void draw(final Canvas canvas) {
+        canvas.drawBitmap(sprite[0], getPosition().getX(), getPosition().getY(), null);
+    }
+}

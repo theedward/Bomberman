77513018336f7684fe35dcdffe9a66433dbd50a9
André Rodrigues <fury252@gmail.com>
2014-04-19 11:36:50 +0100

    The playable interface is not needed because it's too tied with the Agent class.

--- App/app/src/main/java/com/cmov/bomberman/model/Agent.java
+++ App/app/src/main/java/com/cmov/bomberman/model/Agent.java
@@ -1,6 +1,6 @@
 package com.cmov.bomberman.model;
 
-public abstract class Agent implements Drawable, Playable {
+public abstract class Agent implements Drawable {
 	private final Algorithm ai;
 	private Position currentPos;
 
@@ -12,12 +12,12 @@ public abstract class Agent implements Drawable, Playable {
 	protected Position getCurrentPos() {
 		return currentPos;
 	}
-
 	protected void setCurrentPos(Position p) {
 		this.currentPos = p;
 	}
-
 	protected Algorithm getAlgorithm() {
 		return ai;
 	}
+	abstract public void play(State state);
+	abstract public boolean isDestroyed();
 }

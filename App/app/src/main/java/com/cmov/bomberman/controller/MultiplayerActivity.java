2f525c1ed409f4f1b2802ff4b1d11a75f0880fb8
theedward <ed-01@hotmail.com>
2014-04-23 22:09:28 +0100

    Multiplayer Activity done, added stubs for CreateServer and JoinServer activities.

--- App/app/src/main/java/com/cmov/bomberman/controller/MultiplayerActivity.java
+++ App/app/src/main/java/com/cmov/bomberman/controller/MultiplayerActivity.java
@@ -1,8 +1,11 @@
 package com.cmov.bomberman.controller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+
 import com.cmov.bomberman.R;
 
 public class MultiplayerActivity extends Activity {
@@ -20,4 +23,15 @@ public class MultiplayerActivity extends Activity {
 		return true;
 	}
 
+    public void joinServer(View v) {
+        Intent intent = new Intent(MultiplayerActivity.this, JoinServerActivity.class);
+        startActivity(intent);
+    }
+
+    public void createServer(View v) {
+        Intent intent = new Intent(MultiplayerActivity.this, CreateServerActivity.class);
+        startActivity(intent);
+    }
+
 }
+
